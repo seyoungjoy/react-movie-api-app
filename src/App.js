db5848b669d147ce29118c6034f5df82f3f5acf3
seyoungjoy <diddpwl80@gmail.com>
2021-08-26 22:54:51 +0900

    2021.08.26

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React,{useState, useEffect} from 'react';
-
 import Movie from './components/Movie';
 
-const FEATURED_API = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=52bdaebc337db13698b78f5cff54101b&page=1";
+const FEATURED_API = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=52bdaebc337db13698b78f5cff54101b&language=ko&page=1";
 const IMG_API = "https://image.tmdb.org/t/p/w1280"
 const SEARCH_API = "https://api.themoviedb.org/3/search/movie?api_key=52bdaebc337db13698b78f5cff54101b&query="
 
@@ -11,21 +10,54 @@ const SEARCH_API = "https://api.themoviedb.org/3/search/movie?api_key=52bdaebc33
 
 function App() {
   const [movies, setMovies ] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+
 
   useEffect(async () => {
-    fetch(FEATURED_API)
+    getMovies(FEATURED_API);
+  }, [])
+
+  const getMovies = (API) => {
+    fetch(API)
       .then((res) => res.json())
       .then((data) => {
         console.log(data);
         setMovies(data.results);
       });
-  }, [])
+  }
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+
+    if(searchTerm){
+      getMovies(SEARCH_API + searchTerm)     
+
+      setSearchTerm("");
+    }
+  };
+
+  const handleOnChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
 
   return (
-    <div>
-      {movies.length > 0 && movies.map((movie) => 
-      <Movie key={movie.id} {...movie}/>)}
-    </div>
+    <>
+      <header>
+        <form onSubmit={handleOnSubmit}>
+          <input
+            className="search"
+            type="search"
+            placeholder="Search..."
+            value={searchTerm}
+            onChange={handleOnChange}
+          />
+        </form>
+      </header>
+      <div className="movie-container">
+        {movies.length > 0 &&
+          movies.map((movie) => <Movie key={movie.id} {...movie}/>)}
+      </div>
+    </>
   );
 }
 
