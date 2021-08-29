19cdf496fb3062f1433b6b0e364cbe1ddfda27fa
seyoungjoy <diddpwl80@gmail.com>
2021-08-29 21:46:38 +0900

    2021.08.29

--- src/App.js
+++ src/App.js
@@ -5,13 +5,9 @@ const FEATURED_API = "https://api.themoviedb.org/3/discover/movie?sort_by=popula
 const IMG_API = "https://image.tmdb.org/t/p/w1280"
 const SEARCH_API = "https://api.themoviedb.org/3/search/movie?api_key=52bdaebc337db13698b78f5cff54101b&query="
 
-
-
-
 function App() {
   const [movies, setMovies ] = useState([]);
   const [searchTerm, setSearchTerm] = useState('');
-
 
   useEffect(async () => {
     getMovies(FEATURED_API);
